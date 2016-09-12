abf15fa26af6d1d454e3682d11a69b84e8bc9674
Krzysztof Madej <krzysztof.madej@hotmail.com>
2016-09-12 15:43:26 +0200

    Changed assertions in ComposedUnquoteAssertions project

--- Pluralsight/UnitTestingWithFSharp/ComposedUnquoteAssertions/Tests.fs
+++ Pluralsight/UnitTestingWithFSharp/ComposedUnquoteAssertions/Tests.fs
@@ -6,6 +6,11 @@ open Xunit
 open Swensen.Unquote
 open Madko.Samples.HttpApi
 
+let convertsTo<'a> candidate =
+    match box candidate with
+    | :? 'a as converted -> Some converted
+    | _ -> None
+
 [<Fact>]
 let ``Post returns correct result`` () =
     use sut = new FooController ()
@@ -13,5 +18,8 @@ let ``Post returns correct result`` () =
 
     let actual : IHttpActionResult = sut.Post foo
 
-    test <@ actual :? Results.StatusCodeResult @>
-    test <@ (actual :?> Results.StatusCodeResult).StatusCode = HttpStatusCode.Accepted @>
+    test <@ 
+            actual
+            |> convertsTo<Results.StatusCodeResult>
+            |> Option.map (fun x -> x.StatusCode)
+            |> Option.exists ((=) HttpStatusCode.Accepted) @>
