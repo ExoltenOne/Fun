c81790ee7c19a12cea6d5c04b2b45cda8d05f67f
Krzysztof Madej <krzysztof.madej@hotmail.com>
2016-09-12 15:03:52 +0200

    Added ComposedUnquote Assertions project as a part of Pluralsight course - Unit Testing with F#

---
+++ Pluralsight/UnitTestingWithFSharp/ComposedUnquoteAssertions/HttpApi.fs
@@ -0,0 +1,13 @@
+﻿namespace Madko.Samples.HttpApi
+
+open System.Net
+open System.Web.Http
+
+[<CLIMutableAttribute>]
+type FooRendition = { Id : string; Text : string }
+
+type FooController() =
+    inherit ApiController()
+
+    member this.Post (rendition : FooRendition) : IHttpActionResult =
+        this.StatusCode HttpStatusCode.Accepted :> _
---
+++ Pluralsight/UnitTestingWithFSharp/ComposedUnquoteAssertions/Tests.fs
@@ -0,0 +1,17 @@
+﻿module Madko.Samples.HttpApi.Tests
+
+open System.Net
+open System.Web.Http
+open Xunit
+open Swensen.Unquote
+open Madko.Samples.HttpApi
+
+[<Fact>]
+let ``Post returns correct result`` () =
+    use sut = new FooController ()
+    let foo = { Id = "1234"; Text = "Bar" }
+
+    let actual : IHttpActionResult = sut.Post foo
+
+    test <@ actual :? Results.StatusCodeResult @>
+    test <@ (actual :?> Results.StatusCodeResult).StatusCode = HttpStatusCode.Accepted @>
