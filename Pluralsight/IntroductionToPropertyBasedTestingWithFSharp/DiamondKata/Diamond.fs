d57704e1fa3087cbaefd5d3936c3f7ad39fb7a9e
Krzysztof Madej <krzysztof.madej@hotmail.com>
2016-09-13 09:45:00 +0200

    Added assertion for new properties od DiamondKata - Pluralsight course

--- Pluralsight/IntroductionToPropertyBasedTestingWithFSharp/DiamondKata/Diamond.fs
+++ Pluralsight/IntroductionToPropertyBasedTestingWithFSharp/DiamondKata/Diamond.fs
@@ -1,4 +1,22 @@
 ﻿module Madko.Samples.Diamond
 
-let make letter = "Devil's advocate."
+open System
 
+let make letter =
+    let makeLine letterCount letter =
+        let padding = String(' ', letterCount - 1)
+        match letter with
+        | 'A' ->
+            sprintf "%s%c%s" padding letter padding
+        | _ ->
+            let left = sprintf "%c%s" letter padding |> Seq.toList
+            left
+            @ (left |> List.rev |> List.tail)
+            |> List.map string
+            |> List.reduce (sprintf "%s%s")
+
+    let letters = ['A' .. letter]
+    letters
+    @ (letters |> List.rev |> List.tail)
+    |> List.map (makeLine letters.Length)
+    |> List.reduce (fun x y -> sprintf "%s%s%s" x Environment.NewLine y)
