e214dbafb581ba1a9f025dbbb226f2d79da8c65a
Krzysztof Madej <krzysztof.madej@hotmail.com>
2016-09-19 15:09:34 +0200

    Added unit test to RunningJournalApi project with triangulation approach - pluralsight course

---
+++ Pluralsight/OutsideInTestDrivenDevelopment/RunningJournalApi.UnitTests/SimpleWebTokenTests.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading;
+using Microsoft.SqlServer.Server;
+using Xunit;
+
+namespace RunningJournalApi.UnitTests
+{
+    public class SimpleWebTokenTests
+    {
+        [Fact]
+        public void SutIsIteratorOfClaims()
+        {
+            var sut = new SimpleWebToken();
+            Assert.IsAssignableFrom<IEnumerable<Claim>>(sut);
+        }
+
+        [Fact]
+        public void SutYieldsInjectedClaims()
+        {
+            var expected = new[]
+            {
+                new Claim("foo", "bar"),
+                new Claim("bax", "qux"),
+                new Claim("quux", "corge")
+            };
+            var sut = new SimpleWebToken(expected);
+            Assert.True(expected.SequenceEqual(sut));
+            Assert.True(expected.Cast<object>().SequenceEqual(((System.Collections.IEnumerable) sut).OfType<object>()));
+        }
+
+        [Theory]
+        [InlineData(new string[0], "")]
+        [InlineData(new[] {"foo|bar"}, "foo=bar")]
+        [InlineData(new[] { "foo|bar", "baz|qux"}, "foo=bar&baz=qux")]
+        public void ToStringReturnsCorrectResult(string[] keysAndValues, string expected)
+        {
+            var claims = keysAndValues.Select(s => s.Split('|'))
+                .Select(a => new Claim(a[0], a[1])).ToArray();
+            var sut = new SimpleWebToken(claims);
+
+            var actual = sut.ToString();
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ Pluralsight/OutsideInTestDrivenDevelopment/RunningJournalApi/SimpleWebToken.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+
+namespace RunningJournalApi
+{
+    public class SimpleWebToken : IEnumerable<Claim>
+    {
+        private readonly IEnumerable<Claim> claims;
+
+        public SimpleWebToken(params Claim[] claims)
+        {
+            this.claims = claims;
+        }
+
+        public override string ToString()
+        {
+            return this.claims.Select(c => c.Type + "=" + c.Value)
+                .DefaultIfEmpty(string.Empty)
+                .Aggregate((x, y) => x + "&" + y);
+        }
+
+        public IEnumerator<Claim> GetEnumerator()
+        {
+            return this.claims.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
