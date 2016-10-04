18e9c4ff2f1e8369758781398a70918c20eb9d30
Krzysztof Madej <krzysztof.madej@hotmail.com>
2016-10-04 14:06:10 +0200

    Implemented FiniteStateMachine with TopDown approach fo TypeDrivenDevelopmentWithFSharp course

--- Pluralsight/TypeDrivenDevelopmentWithFSharp/PollingConsumer/PollingConsumer.fsx
+++ Pluralsight/TypeDrivenDevelopmentWithFSharp/PollingConsumer/PollingConsumer.fsx
@@ -1,1 +1,90 @@
-﻿// Temporary placeholder
+﻿open System
+
+type Timed<'a> =
+    {
+        Started : DateTimeOffset
+        Stopped : DateTimeOffset
+        Result : 'a
+    }
+    member this.Duration = this.Stopped - this.Started
+
+module Untimed =
+    let map f x =
+        { Started = x.Started; Stopped = x.Stopped; Result = f x.Result }
+
+    let withResult newResult x = map (fun _ -> newResult) x
+
+module Timed =
+    let capture clock x =
+        let now = clock()
+        { Started = now; Stopped = now; Result = x}
+
+    let map clock f x =
+        let result = f x.Result
+        let stopped = clock ()
+        { Started = x.Started; Stopped = stopped; Result = result }
+
+    let timeOn clock f x = x |> capture clock |> map clock f
+
+module Clocks =
+    let machineClock () = DateTimeOffset.Now
+
+    let acclock (start : DateTimeOffset) rate () =
+        let now = DateTimeOffset.Now
+        let elapsed = now - start
+        start.AddTicks (elapsed.Ticks * rate)
+
+    open System.Collections.Generic
+
+    let qlock (q : Queue<DateTimeOffset>) = q.Dequeue
+
+    let seqlock (l : DateTimeOffset seq) = Queue<DateTimeOffset> l |> qlock
+
+// Auxiliary types
+type MessageHandler = unit -> Timed<unit>
+
+// Temporary placeholder
+type todo = unit
+let todo() = ()
+
+/// State data
+
+type ReadyData = Timed<TimeSpan list>
+
+type ReceivedMessageData = Timed<TimeSpan list * MessageHandler>
+
+type NoMessageData = Timed<TimeSpan list>
+
+// States
+
+type PollingConsumer =
+    | ReadyState of ReadyData
+    | ReceivedMessageState of ReceivedMessageData
+    | NoMessageState of NoMessageData
+    | StoppedState
+
+// Transitions
+
+let transisionFromStopped = StoppedState
+
+let transitionFromNoMessage shoulIdle idle (nm : NoMessageData) =
+    if shoulIdle nm
+    then idle () |> Untimed.withResult nm.Result |> ReadyState
+    else StoppedState
+
+let transitionsFromReady shouldPoll poll (r : ReadyData) =
+    if shouldPoll r
+    then
+        let msg = poll ()
+        match msg.Result with
+        | Some h-> msg |> Untimed.withResult (r.Result, h) |> ReceivedMessageState
+        | None -> msg |> Untimed.withResult r.Result |> NoMessageState
+    else StoppedState
+
+let transitionFromReceived (rm : ReceivedMessageData) : PollingConsumer =
+    let durations, handleMessage = rm.Result
+    let t = handleMessage ()
+    let pollDuration = rm.Duration
+    let handleDuration = t.Duration
+    let totalDuration = pollDuration + handleDuration
+    t |> Untimed.withResult (totalDuration :: durations) |> ReadyState
