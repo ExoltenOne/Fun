b12aab11582800aed6baf95e96c2f448e4d4f758
Krzysztof Madej <krzysztof.madej.1987@gmail.com>
2018-12-27 21:10:44 +0100

    Added FunctionalConcurency and Tuples comparison projects

---
+++ Experiments/Tuples/TuplesBenchmark.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using BenchmarkDotNet.Attributes;
+
+namespace Tuples
+{
+    [MemoryDiagnoser]
+    public class TuplesBenchmark
+    {
+        private const int NumberOfIterations = 10000;
+
+        [Benchmark]
+        public void ReferenceTuple()
+        {
+            long counter = 0L;
+            for (var n = 0; n < NumberOfIterations; n++)
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    var tuple = Tuple.Create("string", 10L, 777m, 45.45);
+
+                    counter += tuple.Item2;
+                }
+            }
+
+            Console.WriteLine(counter);
+        }
+
+        [Benchmark]
+        public void ValueTuple()
+        {
+            long counter = 0L;
+            for (var n = 0; n < NumberOfIterations; n++)
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    var tuple = ("string", 10L, 777m, 45.45);
+
+                    counter += tuple.Item2;
+                }
+            }
+
+            Console.WriteLine(counter);
+        }
+    }
+}
